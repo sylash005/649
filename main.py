1cef93de5c29b9a6de7717d87db95320b3209802
Diego Molla <diego.molla-aliod@mq.edu.au>
2019-03-22 18:17:07 +1100

    Documentation strings in main.py

--- main.py
+++ main.py
@@ -12,22 +12,64 @@ from database import DATABASE_NAME, create_tables
 
 
 def read_relations(db, filename):
-    """Read the relations.csv file and store records in the database"""
+    """Store the relations listed in filename into the database
+    - db      : a connection to a database.
+    - openfile: CSV file open for reading and holding a relation per line.
+    This function does not return any values. After executing this function, each row of the CSV file
+    will be stored as a relation in the database.
+
+    Example of use:
+    >>> db = sqlite3.connect(DATABASE_NAME)
+    >>> with open('relations.csv') as f:
+    >>>    read_relations(db, f)
+    """
     pass
 
 
 def read_locations(db, filename):
-    """Read locations.csv and store records in the database"""
+    """Store the locations listed in the open file into the database
+    - db      : a connection to a database.
+    - openfile: CSV file open for reading and holding a location per line.
+    This function does not return any values or print anything on screen. After executing this function,
+    each row of the CSV file will be stored as a location in the database.
+
+    Example of use:
+    >>> db = sqlite3.connect(DATABASE_NAME)
+    >>> read_locations(db, open('locations.csv'))
+    """
     pass
 
 
 def read_stock(db, filename):
-    """Read stock information from the HTML file and store in the database"""
+    """Read the products from the open file and store them in the database
+    - db      : a connection to a database.
+    - openfile: HTML file open for reading and listing products.
+    This function does not return any values or print anything on screen. After executing this function,
+    the products found in the HTML file will be stored as product records in the database.
+
+    Example of use:
+    >>> db = sqlite3.connect(DATABASE_NAME)
+    >>> read_stock(db, open('index.html'))
+    """
     pass
 
 
 def report(db, outfile):
-    """Run the required report over the database"""
+    """Generate a database report and store it in outfile
+    - db      : a connection to a database
+    - openfile: a CSV file open for writing
+    This function does not return any values or print anything on screen. After executing this function,
+    the file outfile will contain the product information, one row in the CSV file per product. Each row must
+    contain the following information:
+      - description
+      - price (including the currency symbol)
+      - amount in stock
+      - store location
+
+    Example of use:
+    >>> db = sqlite3.connect(DATABASE_NAME)
+    >>> report(db, open('report.csv', 'w'))
+    """
     pass
 
 
